9e88a7362406d2ef1b41058bc5bb93acb389ed5b
Antonio Vitor <vitorferreiraa@outlook.com.br>
2025-06-17 11:49:20 -0300

    Update index.ts

--- Typescript/002 - Tipos/002 - Array e Objeto/index.ts
+++ Typescript/002 - Tipos/002 - Array e Objeto/index.ts
@@ -6,4 +6,30 @@ let array1: [object] = [
     condicao: true,
     }
 ];
-let array2: Array<string | number | boolean | object> = ["Denner","Troquatte",{nome:"Den"},];
+let array2: Array<string | number | boolean | object> = ["Denner","Troquatte",{nome:"Den"},]; // operador | (or) aceita tais condições no array
+
+
+//console.log(array1)
+//console.log(array2);
+
+let obj1: {nome: string, sobrenome:string,idade:number,deuBom:boolean} = {
+    nome:"Dener",
+    sobrenome:"Troquatte",
+    idade:123,
+    deuBom: true,
+};
+
+let obj2: Array<{
+    nome: string;
+    sobrenome: string;
+    idade: number;
+    deuBom: boolean;
+}> = [
+    {
+    nome:"Dener",
+    sobrenome:"Troquatte",
+    idade:123,
+    deuBom: true,
+}];
+
+console.log(...obj2);
