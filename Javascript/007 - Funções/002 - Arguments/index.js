151fc202f038674f050b4dffe8a69a964c97d79f
Antonio Vitor <113688321+avfs01@users.noreply.github.com>
2025-06-17 08:33:47 -0300

    Update index.js

--- Javascript/007 - Funções/002 - Arguments/index.js
+++ Javascript/007 - Funções/002 - Arguments/index.js
@@ -1,3 +1,12 @@
+/*
+O objeto arguments é como um objeto Array correspondendo aos argumentos passados para uma função.
+Sintaxe: arguments
+
+O objeto arguments é uma variável local disponível dentro de todas as funções. 
+Você pode referenciar os argumentos de uma função dentro da função usando o objeto arguments. 
+Esse objeto contém um registro para cada argumento fornecido para a função, com o índice do primeiro registro começando em 0.
+*/
+
 function soma (num1, num2){
     if(typeof num1 === "number" && typeof num2 === "number"){
         return num1+num2;
@@ -13,4 +22,9 @@ function subtracao(){
     return arguments; // O retorno arguments retorna os argumentos ou os parâmetros passados dentro da função.
 }
 
-console.log(subtracao(1,2, "Abc"));
+
+
+
+/* O objeto argumentos não é um Array. É similar a um Array, mas não possui as propriedades de Array, exceto length. 
+Por exemplo, ele não possui o método pop. Entretanto, ele pode ser convertido em um Array real
+*/
