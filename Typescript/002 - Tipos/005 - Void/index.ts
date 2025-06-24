c2deda9dde9272ea547cd17d7fd167eaef0849a6
Antonio Vitor <vitorferreiraa@outlook.com.br>
2025-06-24 11:51:07 -0300

    Create index.ts

---
+++ Typescript/002 - Tipos/005 - Void/index.ts
@@ -0,0 +1,18 @@
+// Uma void é uma função que não possui valor de retorno
+
+// sintaxe
+
+// function semRetorno (a: string) => void
+
+let pedido = (msg: string): void =>{
+    console.log(msg);
+
+    // return msg; Ocasionaria um erro pois a void não retorna nada
+}
+pedido("Deu bom!");
+
+/*
+ Ausência de Valor: Indica que a função não retorna nada útil.
+ Compatibilidade com undefined: Em modo não estrito (strictNullChecks: false), void também aceita null.
+ Diferente de undefined: Uma função void pode não ter return, mas uma função : undefined precisa retornar undefined explicitamente.
+*/
