d5e7a52c41ed42ea58092bf4cb11b92784cb5913
Antonio Vitor <vitorferreiraa@outlook.com.br>
2025-06-30 12:11:50 -0300

    Create index.ts

---
+++ Typescript/003 - Classes/006 - Abstract/index.ts
@@ -0,0 +1,16 @@
+/*
+    Classes, métodos e campos no Typescript podem ser abstratos.
+
+    Um método abstrato ou campo abstrato é aquele que não teve
+    uma implementação fornecida.
+
+    O papel das classes abstratas é servir como uma classe base para subclasses
+    que implementam todos os membros abstratos.
+
+    Quando uma classe
+*/
+
+class Pessoa {
+    protected nome: string = " ";
+    protected idade: number = 0;
+}
